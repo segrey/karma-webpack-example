1388f34a575803a5294c434066ff1876b3d6676a
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2019-06-05 03:16:59 +0300

    add reporter and set LOG_INFO

--- karma.conf.js
+++ karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function(config) {
     webpackMiddleware: {
       noInfo: false
     },
-    reporters: [],
+    reporters: ['progress'],
 
     // web server port
     port: 9876,
@@ -34,7 +34,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
 
 
     // enable / disable watching file and executing tests whenever any file changes
